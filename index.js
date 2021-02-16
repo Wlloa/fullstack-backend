827fd7f97ec3d9019c7feb734134f2a173b1a5ca
Wilber Ulloa <wilber@fonoma.com>
2021-02-15 20:40:40 -0500

    exerc 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { json } = require('express')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 let phonebook =  [
     {
@@ -26,9 +28,17 @@ let phonebook =  [
 
 ]
 
+const PORT = 3001
+
+const generateId = () => {
+    const maxId = phonebook.length > 0 
+    ? Math.max(...phonebook.map(p => p.id)) : 0
+    
+    return maxId + 1
+}
+
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${phonebook.length} people <p>${new Date()}<p/>`)
-    console.log(request)
 })
 
 app.get('/api/persons', (request, response) => {
@@ -52,8 +62,25 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if(!body.name) {
+        return response.status(404).json({
+            error: 'missing contact name'
+        })
+    }
 
-const PORT = 3001
+    const contact = {
+        id: generateId(),
+        name: body.name,
+        number: body.number || ''
+    }
+
+    phonebook = phonebook.concat(contact)
+    response.json(contact)
+
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
