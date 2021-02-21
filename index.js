f4523058c980a7b53e3b32ba3f4dccbd9debe106
Wilber Ulloa <wilber@fonoma.com>
2021-02-21 18:47:41 -0500

    exerc 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { json, request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 morgan.token('body', function getBody (req){
     return JSON.stringify(req.body)
