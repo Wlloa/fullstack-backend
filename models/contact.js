0feec87eebb73d6c3f09705eb2f2f263b003cc46
Wilber Ulloa <wilber@fonoma.com>
2021-02-25 00:42:39 -0500

    fixed bug with schema set function

--- models/contact.js
+++ models/contact.js
@@ -13,7 +13,7 @@ const contactSchema = new mongoose.Schema({
     number: String,
 })
 
-contactSchema.set({
+contactSchema.set('toJSON',{
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
